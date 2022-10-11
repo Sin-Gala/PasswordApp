6ac513d7bfeeceaa0ed800240541056ac12bcbf3
SinGala67 <galahane.rouille@gmail.com>
2022-10-11 16:59:55 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PasswordApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInteraction userInteraction = new UserInteraction();
+
+            Console.WriteLine("Welcome to your Password App!");
+            userInteraction.CallMainMenu();
+        }
+    }
+}
---
+++ UserInteraction.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace PasswordApp
+{
+    public class UserInteraction
+    {
+        bool launched = false;
+
+        public void CallMainMenu()
+        {
+            if (launched)
+                Console.Clear();
+            else
+                launched = true;
+
+            Console.WriteLine("What do you wish to do?");
+            Console.WriteLine("\n1: See a Password \n2: Add a Password \n3: Edit a Password " +
+                "\n4: Delete a Password \n5 Delete all datas");
+
+            switch (Console.ReadLine())
+            {
+                case "1": // See Password
+                    SeePassword();
+                    break;
+                case "2": // Add Password
+                    AddPassword();
+                    break;
+                case "3": // Edit Password
+                    EditPassword();
+                    break;
+                case "4": // Delete Password
+                    RemovePassword();
+                    break;
+                case "5": // Delete all datas
+                    DeleteAll();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid command");
+                    Console.ReadKey();
+                    CallMainMenu();
+                    break;
+            }
+        }
+
+        private void SeePassword()
+        {
+            Console.WriteLine("See");
+        }
+
+        private void AddPassword()
+        {
+            Console.WriteLine("Add");
+        }
+
+        private void RemovePassword()
+        {
+            Console.WriteLine("Remove");
+        }
+
+        private void EditPassword()
+        {
+            Console.WriteLine("Edit");
+        }
+
+        private void DeleteAll()
+        {
+            Console.WriteLine("Delete all");
+        }
+    }
+}
